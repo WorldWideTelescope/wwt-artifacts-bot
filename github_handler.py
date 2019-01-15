617c50fd1f93b71d7e55da2018bb50dc1c54f7ce
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-01-15 17:09:18 +0000

    Updated artifact handler

--- github_handler.py
+++ github_handler.py
@@ -1,5 +1,4 @@
-import pprint
-
+import requests
 from flask import current_app
 
 from baldrick.github.github_api import PullRequestHandler
@@ -9,19 +8,41 @@ from baldrick.blueprints.github import github_webhook_handler
 @github_webhook_handler
 def handle_pull_requests(repo_handler, payload, headers):
 
-    event = headers['X-GitHub-Event']
-
-    pprint.pprint(payload)
-
-    print(event, payload['action'])
-
-    if event == 'pull_request':
-        number = payload['pull_request']['number']
-    elif event == 'issues':
-        number = payload['issue']['number']
-    else:
-        return "Not an issue or pull request"
-
     pr_handler = PullRequestHandler(repo_handler.repo, number, repo_handler.installation)
 
-    pr_handler.submit_comment('Testing')
+    event = headers['X-GitHub-Event']
+
+    # We are only intersted in checks that are completed
+    if event != 'check_run':
+        return
+
+    # We are only interested in checks that have been completed by Azure
+    if payload['check_run']['app']['name'] != 'Azure Pipelines':
+        return
+
+    if payload['check_run']['conclusion'] != 'success':
+        return
+
+    details_url = payload['check_run']['details_url']
+
+    # details_url is a URL of the form:
+    # https://dev.azure.com/thomasrobitaille/<pipeline>/_build/results?buildId=<build_id>
+    if '?buildID=' not in details_url or '/_build/' not in details_url:
+        print('ERROR: URL did not match pattern: ' + details_url)
+        return
+
+    build_id = details_url.split('buildId=')[1]
+    base_url = details_url.split('_build')[0]
+    artifacts_url = f'{base_url}/_apis/build/builds/{build_id}/artifacts'
+
+    artifacts = requests.get(artifacts_url).json()
+
+    if artifacts['count'] > 0:
+
+        message = 'The Azure Pipelines build produced the following artifacts:\n\n'
+
+        for artifact in artifacts['value']:
+            download_url = artifacts_url + '?artifactName={name}&fileId={data}&fileName={name}&api-version=5.0-preview.5'.format(name=artifact['name'], data=artifact['resource']['name'])
+            message += '* [{name}]({url})\n'.format(name=artifact['name'], url=download_url)
+
+        pr_handler.submit_comment(message)
